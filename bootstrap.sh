5adc3279ca3d260e13724e7dbb8fbcc6688e7bc2
Gilbert <gilbertwong96@icloud.com>
2024-09-27 15:01:09 +0800

    Add bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+ln -sFfi $(PWD)/.tmux.conf ~/.tmux.conf
+ln -sFfi $(PWD)/config.fish ~/.config/fish/config.fish
+ln -sFfi $(PWD)/kitty.fish ~/.config/kitty/kitty.conf
+ln -sFfi $(PWD)/starship.toml ~/.config/starship.toml
