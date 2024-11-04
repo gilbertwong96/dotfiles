bd7dfc00755182c2a67a0b7098cd790fb6fbfcba
Gilbert <gilbertwong96@icloud.com>
2024-11-04 21:21:32 +0800

    Update .gitignore

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,5 +2,6 @@
 
 ln -sFfi $(PWD)/.tmux.conf ~/.tmux.conf
 ln -sFfi $(PWD)/config.fish ~/.config/fish/config.fish
-ln -sFfi $(PWD)/kitty.fish ~/.config/kitty/kitty.conf
+# Hardlink for kitty config
+ln -Ffi $(PWD)/kitty.conf ~/.config/kitty/kitty.conf
 ln -sFfi $(PWD)/starship.toml ~/.config/starship.toml
