4ad7ecb97135af9c7079c6168413f42d234a655a
Gilbert <gilbertwong96@icloud.com>
2025-10-24 18:13:19 +0800

    Add LIBRARY_PATH

--- config.fish
+++ config.fish
@@ -2,6 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+set -gx LIBRARY_PATH /opt/homebrew/opt/libgccjit/lib/gcc/current
+
 ########################
 ### GLOBAL VARIABLES ###
 ########################
